f2baaa3c3d3c766ae87899e8efece0defe40b2e6
Harry <ary80@live.fr>
2025-03-06 07:25:05 +0100

    App: import scss and delete comments

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes, Link, BrowserRouter } from "react-router-dom"; // Import React Router components
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+
+import { BrowserRouter,Route, Routes } from "react-router-dom"; // Import React Router components
+// import useState from "react" ; 
+// import useEffect from "react";
+// import React from "react";
+
+
 import './App.css'
-// import * as Components from '/assets/component'
+
 import About from './About.jsx' 
 import Contact from './Contact.jsx'
 import Home from './Home.jsx'
 import Error from './Error.jsx'
-import Header from './component/Header.jsx';
+import Header from './component/header.jsx';
 import Footer from './component/footer.jsx';
+// import 'main.scss'
+import './assets/styles/main.scss'
+
+
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -21,7 +28,12 @@ function App() {
     <Header/> 
       <div className="container mx-auto mt-6">
         <Routes>
+
+          
           <Route path="/" element={<Home />} />
+
+
+
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<Error />} /> {/* Catch-all for 404 */}

3d2e6d3dad1250ab136c24c396e9d7135f2bd4ee
Harry <ary80@live.fr>
2025-03-06 07:22:52 +0100

    Home:Page with every appartment from the database
    Using useState and UseEffect to make sure everything is on date.

--- my-react-app/src/Home.jsx
+++ my-react-app/src/Home.jsx
@@ -1,19 +1,49 @@
+import { useState, useEffect } from "react"; // ✅ Correct Import
+
 function Home() {
-    return <h2>Home Page</h2>;
-    // <header>
+  // const [count, set_count] = useState(0);
 
-    // </header>
-    // <div>
+  //  question sur les useState and more
+  const [logements, setLogements] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    // </div>
-    // <main>
+   useEffect(() => {
+    const fetchLogements = async () => {
+      try {
+        const response = await fetch("../public/logements.json"); // Fetch from public folder
+        if (!response.ok) throw new Error("Failed to load data");
 
-    // </main>
-  };
+        const data = await response.json();
+        console.log(data);
+        setLogements(data);
+      } catch (error) {
+        console.error("Error fetching logements:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  export default Home;
+    fetchLogements();
+  }, []);
 
+  if (loading) return <p>Loading...</p>;
 
+  return (
+    <div>
+      <h2>Liste des Logements</h2>
+      {logements.map((lg) => (
+        // lg.title
+        <article key={lg.id} >
+         
+          <div>
+            <h3>{lg.title}</h3>
+            <img src={lg.cover} alt={lg.title} />
+            </div>
+        </article>
+        ))}
 
+    </div>
+  );
+}
 
-  
+export default Home;

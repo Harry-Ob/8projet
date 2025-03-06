c23d19eb21c21b032240af4c2ac948dee766b276
Harry <ary80@live.fr>
2025-03-06 07:21:47 +0100

    Error: Page Error

--- my-react-app/src/Error.jsx
+++ my-react-app/src/Error.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom"; // Import React Router components
-
+import './assets/styles/error.scss'
 
 function Error() {
   return (
     <div>     
     <h1 className="err">404</h1> 
     <h3> Oups! La page que vous demandez nexiste pas.</h3>
-    <li><Link to="/">retourner sur la page d&apos;accueil</Link></li>
+    <Link to="/">retourner sur la page d&apos;accueil</Link>
     </div>
 
 

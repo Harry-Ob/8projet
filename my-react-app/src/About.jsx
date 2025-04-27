e4e238837d3a52c1cce6faf2a620523142d39b77
Harry <ary80@live.fr>
2025-04-27 14:31:09 +0200

    About.jsx: v_0.1 background img done -> v_0 is for final version with comment this version awaits Collapsible element, header corrected and to see if the behaviour at the end match the one needed from figma

--- my-react-app/src/About.jsx
+++ my-react-app/src/About.jsx
@@ -10,26 +10,28 @@ import bg_img from "./assets/img/image2.jpeg" ;
 
 function About() {
     return (
-              <div className="rounded-xl border-2 border-blue-500 overflow-hidden ">
+              <div className="rounded-xl border-2 border-blue-500 overflow-hidden  ">
                 {/* w-[335px] h-[111px] */}
                 {/* tricks with absolute and relative to make sure ur text stays in the middle no matter what  */}
                 <div  className="relative 
                 sm:h-48 sm:w-full
-                md:h-56 md:container md:mx-auto md:w-full
-                lg:h-56 lg:max-w-7xl 
+                md:h-56   md:mx-auto md:w-full
+                lg:h-56 
                 flex items-center justify-center
                 bg-cover bg-center 
-                before:absolute before:inset-0 before:bg-black/60 before:mix-blend-darken "
+                before:absolute before:inset-0 before:bg-black/60 before:mix-blend-darken 
+                h-[111px]"
+                // lg:max-w-7xl  md:container => width and height to make sure it fits and everything is fine. 
                 style={{
                   backgroundImage: `url(${bg_img})`, // Change this!
                 }}>
-                <h2 className="relative z-10 w-full 
+                {/* <h2 className="relative z-10 w-full 
                 pb-[50px] pt-[50px]
                 text-2xl font-bold 
                 mb-6 mt-6 
                  px-4">
-              {/* Chez vous, partout et ailleurs */}
-            </h2>
+              Chez vous, partout et ailleurs
+            </h2> */}
       
                 </div>
               </div>

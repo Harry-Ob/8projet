cf375a219971d15899ea14a9b9b9afed89ef9d06
Harry <ary80@live.fr>
2025-04-28 17:20:03 +0200

    Collapsible.jsx: first commit

---
+++ my-react-app/src/component/Collapsible.jsx
@@ -0,0 +1,15 @@
+
+
+function Collapsible ({ list_titles, list_content }) {
+    const [open_collapsible,set_open_collapsible] = useState(null);
+
+
+    return (
+        <div></div>
+    );
+
+};
+
+
+
+export default Collapsible ; 

443f9eb129a9fcfd3ba835489fcbccfffc98dd89
Harry <ary80@live.fr>
2025-04-28 15:13:19 +0200

    Error.jsx: css done with tailwind

--- my-react-app/src/Error.jsx
+++ my-react-app/src/Error.jsx
@@ -1,16 +1,17 @@
 import { Link } from "react-router-dom"; // Import React Router components
-import './assets/styles/error.scss'
+import "./assets/styles/error.scss";
 
 function Error() {
   return (
-    <div>     
-    <h1 className="err">404</h1> 
-    <h3> Oups! La page que vous demandez nexiste pas.</h3>
-    <Link to="/">retourner sur la page d&apos;accueil</Link>
+    <div className="flex flex-col items-center justify-center min-h-screen text-center">
+      <h1 className="text-[120px] md:text-[288px] font-bold text-[#FF6060]">
+        404
+      </h1>
+      <h3 className="text-lg font-medium md:text-2xl text-[#FF6060] leading-[1.43] mt-28"> 
+        Oups! La page que vous demandez n&apos;existe pas.
+        </h3>
+      <Link to="/" className="mt-28 text-sm md:text-base text-black hover:text-black underline">Retourner sur la page d&apos;accueil</Link>
     </div>
-
-
-
   );
 }
 

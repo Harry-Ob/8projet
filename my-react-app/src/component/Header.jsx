b3104471053f3fc6a6173fd304644954f1aac226
Harry <ary80@live.fr>
2025-08-04 18:39:39 +0200

    Header.jsx: css and useNavigate added

--- my-react-app/src/component/Header.jsx
+++ my-react-app/src/component/Header.jsx
@@ -1,14 +1,16 @@
-
+import  { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom"; // Import React Router components
 import logo from "../assets/img/LOGO-header-kai.png";
 // import "../assets/styles/header.scss"
 
+
 function Header() {
+  const navigate = useNavigate(); 
   return (
     <header className="max-w-6xl mx-auto px-4 py-10">
       <nav>
         <div className="flex items-center justify-between">
-          <img src={logo} alt="header-logo" className="h-12 w-auto"/>
+          <img src={logo} alt="header-logo" className="h-12 w-auto" onClick={() => navigate("/")}/>
           {/* nav_link could be deleted here as much as logo which was used in img */}
           <div className="flex gap-6 sm:flex-row sm:gap-4 nav_link">
             <Link to="/" >Accueil</Link>

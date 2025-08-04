4eb87f5226fc9b3fbe51592e2091566aec8f7c21
Harry <ary80@live.fr>
2025-08-04 18:43:29 +0200

    About.jsx: var, tailwind + Collapsible call
    var : with the information needed for the Collapsible
    tailwind : css changed with the structure
    function map : Calling the Collapsible with the arguments needed

--- my-react-app/src/About.jsx
+++ my-react-app/src/About.jsx
@@ -1,19 +1,36 @@
-import bg_img from "./assets/img/image2.jpeg" ;
-// function t(){
-  
-//   // <Collapsible/> 
-//   return (
-    
-//   <h2>About Page</h2>
-//   )
-// }
+import bg_img from "./assets/img/image2.jpeg";
+import Collapsible from "./component/Collapsible";
 
 function About() {
-    return (
-              <div className="rounded-xl border-2 border-blue-500 overflow-hidden  ">
-                {/* w-[335px] h-[111px] */}
-                {/* tricks with absolute and relative to make sure ur text stays in the middle no matter what  */}
-                <div  className="relative 
+  const aproposData = [
+    {
+      title: "Fiabilité",
+      content:
+        "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.",
+    },
+    {
+      title: "Respect",
+      content:
+        "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
+    },
+    {
+      title: "Service",
+      content:
+        "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+    },
+    {
+      title: "Sécurité",
+      content:
+        "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+    },
+  ];
+
+  return (
+    <div className="flex flex-col">
+      <div className="rounded-xl border-2 border-blue-500 overflow-hidden  mb-7 ">
+        {/* tricks with absolute and relative to make sure ur text stays in the middle no matter what  */}
+        <div
+          className="relative 
                 sm:h-48 sm:w-full
                 md:h-56   md:mx-auto md:w-full
                 lg:h-56 
@@ -21,21 +38,20 @@ function About() {
                 bg-cover bg-center 
                 before:absolute before:inset-0 before:bg-black/60 before:mix-blend-darken 
                 h-[111px]"
-                // lg:max-w-7xl  md:container => width and height to make sure it fits and everything is fine. 
-                style={{
-                  backgroundImage: `url(${bg_img})`, // Change this!
-                }}>
-                {/* <h2 className="relative z-10 w-full 
-                pb-[50px] pt-[50px]
-                text-2xl font-bold 
-                mb-6 mt-6 
-                 px-4">
-              Chez vous, partout et ailleurs
-            </h2> */}
-      
-                </div>
-              </div>
-    )
-  }
+          style={{
+            backgroundImage: `url(${bg_img})`, // Change this!
+          }}
+        >
+          {/* // the first argument is an array 
+// the 2nd is an array : with a string and an array 
+// list_titles, list_content */}
+        </div>
+      </div>
 
-  export default About;
+
+     { aproposData.map( (item,index) => <Collapsible key={index} title={item.title} content={item.content}/>)}
+    </div>
+  );
+}
+
+export default About;

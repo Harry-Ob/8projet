3d990fb7a1a34b488ff1b2d3b353452a54772579
Harry <ary80@live.fr>
2025-10-06 19:08:12 +0200

    Footer.jsx: indentation changes

--- my-react-app/src/component/Footer.jsx
+++ my-react-app/src/component/Footer.jsx
@@ -8,13 +8,8 @@ function Footer () {
             <div className="container-footer">
             <img src={logo} alt="header-logo" className="logo" />
             <p className='copyright'>© 2020 Kasa. All rights reserved</p>
-
             </div>
-
         </div>
-
     );
-
 } 
-
 export default Footer; 

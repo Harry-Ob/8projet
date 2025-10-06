8ceddfeb66b2a3aa549fb538eaf0d1c13ab74ac4
Harry <ary80@live.fr>
2025-10-06 18:59:38 +0200

    App.jsx: import cleaner, css and browserRouter add

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,53 +1,34 @@
-
-import { BrowserRouter,Route, Routes } from "react-router-dom"; // Import React Router components
-// import useState from "react" ; 
-// import useEffect from "react";
-// import React from "react";
-
-
-import './App.css'
-
-import About from './About.jsx' 
-import Contact from './Contact.jsx'
-import Home from './Home.jsx'
-import Error from './Error.jsx'
-import Header from './component/header.jsx';
-import Footer from './component/footer.jsx';
-// import 'main.scss'
-import './assets/styles/main.scss'
-
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"; // Import React Router components
+import "./App.css";
+import About from "./About.jsx";
+// import Contact from "./Contact.jsx";
+import Home from "./Home.jsx";
+import Error from "./Error.jsx";
+import Header from "./component/Header.jsx";
+import Footer from "./component/footer.jsx";
+import "./assets/styles/main.scss";
+import Fiche_logement from "./Fiche_logement.jsx";
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <>
-    
-    <BrowserRouter>
-    <Header/> 
-      <div className="container mx-auto mt-6">
-        <Routes>
-
-          
-          <Route path="/" element={<Home />} />
-
-
-
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<Error />} /> {/* Catch-all for 404 */}
-        </Routes>
-      </div>
-    </BrowserRouter>
-
-
-     <Footer/>
+      <BrowserRouter>
+        
+        <div className=" mx-auto mt-6 px-8">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            {/* <Route path="/contact" element={<Contact />} /> */}
+            <Route path="*" element={<Error />} /> {/* Catch-all for 404 */}
+            <Route path="/404" element={<Error />} />
+            <Route path="/logement/:id" element={<Fiche_logement />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+      <Footer />
     </>
-    
-
-    
-  )
+  );
 }
 
-export default App
+export default App;

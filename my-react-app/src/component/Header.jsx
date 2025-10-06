2b460bfa1ff284a2c30943932826ac8a1c6572dc
Harry <ary80@live.fr>
2025-10-06 19:09:00 +0200

    Header.jsx: tailwind changes

--- my-react-app/src/component/Header.jsx
+++ my-react-app/src/component/Header.jsx
@@ -7,11 +7,10 @@ import logo from "../assets/img/LOGO-header-kai.png";
 function Header() {
   const navigate = useNavigate(); 
   return (
-    <header className="max-w-6xl mx-auto px-4 py-10">
+    <header className="max-w mx-auto px-4 py-10">
       <nav>
         <div className="flex items-center justify-between">
           <img src={logo} alt="header-logo" className="h-12 w-auto" onClick={() => navigate("/")}/>
-          {/* nav_link could be deleted here as much as logo which was used in img */}
           <div className="flex gap-6 sm:flex-row sm:gap-4 nav_link">
             <Link to="/" >Accueil</Link>
             <Link to="/about"> A propos</Link>

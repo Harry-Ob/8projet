fc5397681614cfa559a49eb1f0e7c2ba8ed6315b
Harry <ary80@live.fr>
2025-10-07 17:13:53 +0200

    Slider.jsx: refactor the condition

--- my-react-app/src/component/Slider.jsx
+++ my-react-app/src/component/Slider.jsx
@@ -24,30 +24,34 @@ function Slider ({list_img}) {
         alt={`Slide ${currentImage + 1}`}
         className="w-full h-96 object-cover rounded-lg"
       />
-        {len_pic !== 1 && (
+
+      { len_pic !== 1 && (
+      <>
+       
       <button
         onClick={prevImage}
         className="absolute left-4 top-1/2 transform -translate-y-1/2 text-white  "
       >
         <i className="fa-solid fa-sharp fa-chevron-down rotate-90 text-[1.5rem] md:text-[3rem]"></i>
       </button>
-        )} 
+        
      
-     {len_pic !== 1 && (
+     
       <button
         onClick={nextImage}
         className="absolute right-4 top-1/2 transform -translate-y-1/2 text-white  "
       >
         <i className="fa-solid fa-sharp fa-chevron-up rotate-90 text-[1.5rem] md:text-[3rem]"></i>
       </button>
-      )} 
       
-      {len_pic !== 1 && (
+      
+     
       <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-white px-3 py-1">
         {currentImage + 1}/{len_pic}
       </div>
-       )} 
-
+      
+      </>
+      )}
     </div>
     ); 
 }

9b2ae278f3362f2927ae4f656356e44d6297f390
Harry <ary80@live.fr>
2025-10-07 17:48:17 +0200

    Footer.jsx:indentation and clean comments

--- my-react-app/src/component/Footer.jsx
+++ my-react-app/src/component/Footer.jsx
@@ -1,15 +1,14 @@
-import logo from '../assets/img/kasa-logo-footer.png' ;
-import "../assets/styles/footer.scss"
+import logo from "../assets/img/kasa-logo-footer.png";
+import "../assets/styles/footer.scss";
 
-function Footer () {
-
-    return (
-        <div className="footer">
-            <div className="container-footer">
-            <img src={logo} alt="header-logo" className="logo" />
-            <p className='copyright'>© 2020 Kasa. All rights reserved</p>
-            </div>
-        </div>
-    );
-} 
-export default Footer; 
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="container-footer">
+        <img src={logo} alt="header-logo" className="logo" />
+        <p className="copyright">© 2020 Kasa. All rights reserved</p>
+      </div>
+    </div>
+  );
+}
+export default Footer;

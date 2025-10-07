6b867f6d0459d05ec5bcba4acfb42e1efcd9cd8d
Harry <ary80@live.fr>
2025-10-07 19:17:01 +0200

    Slider.jsx: indentation, clean comments and refact
    refactor => if with "fragment REACT"

--- my-react-app/src/component/Slider.jsx
+++ my-react-app/src/component/Slider.jsx
@@ -1,14 +1,13 @@
 import PropTypes from "prop-types";
 import { useState } from "react";
 
-
-function Slider ({list_img}) {
+function Slider({ list_img }) {
   // To manage slider index
   const [currentImage, setCurrentImage] = useState(0);
-  const len_pic = list_img.length ;
+  const len_pic = list_img.length;
 
   const nextImage = () => {
-    setCurrentImage((next) => (next + 1) %len_pic );
+    setCurrentImage((next) => (next + 1) % len_pic);
   };
 
   const prevImage = () => {
@@ -25,38 +24,32 @@ function Slider ({list_img}) {
         className="w-full h-96 object-cover rounded-lg"
       />
 
-      { len_pic !== 1 && (
-      <>
-       
-      <button
-        onClick={prevImage}
-        className="absolute left-4 top-1/2 transform -translate-y-1/2 text-white  "
-      >
-        <i className="fa-solid fa-sharp fa-chevron-down rotate-90 text-[1.5rem] md:text-[3rem]"></i>
-      </button>
-        
-     
-     
-      <button
-        onClick={nextImage}
-        className="absolute right-4 top-1/2 transform -translate-y-1/2 text-white  "
-      >
-        <i className="fa-solid fa-sharp fa-chevron-up rotate-90 text-[1.5rem] md:text-[3rem]"></i>
-      </button>
-      
-      
-     
-      <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-white px-3 py-1">
-        {currentImage + 1}/{len_pic}
-      </div>
-      
-      </>
+      {len_pic !== 1 && (
+        <>
+          <button
+            onClick={prevImage}
+            className="absolute left-4 top-1/2 transform -translate-y-1/2 text-white  "
+          >
+            <i className="fa-solid fa-sharp fa-chevron-down rotate-90 text-[1.5rem] md:text-[3rem]"></i>
+          </button>
+
+          <button
+            onClick={nextImage}
+            className="absolute right-4 top-1/2 transform -translate-y-1/2 text-white  "
+          >
+            <i className="fa-solid fa-sharp fa-chevron-up rotate-90 text-[1.5rem] md:text-[3rem]"></i>
+          </button>
+
+          <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 text-white px-3 py-1">
+            {currentImage + 1}/{len_pic}
+          </div>
+        </>
       )}
     </div>
-    ); 
+  );
 }
 
 Slider.propTypes = {
-  list_img: PropTypes.arrayOf(PropTypes.string).isRequired
+  list_img: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
-export default Slider ; 
+export default Slider;

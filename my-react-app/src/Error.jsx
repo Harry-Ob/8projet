4d8fa4d5b2fcc8f9253bdf95606da5c325f361de
Harry <ary80@live.fr>
2025-10-07 17:23:40 +0200

    Error.jsx:indentation and clean comments

--- my-react-app/src/Error.jsx
+++ my-react-app/src/Error.jsx
@@ -7,10 +7,15 @@ function Error() {
       <h1 className="text-[120px] md:text-[288px] font-bold text-[#FF6060]">
         404
       </h1>
-      <h3 className="text-lg font-medium md:text-2xl text-[#FF6060] leading-[1.43] mt-28"> 
+      <h3 className="text-lg font-medium md:text-2xl text-[#FF6060] leading-[1.43] mt-28">
         Oups! La page que vous demandez n&apos;existe pas.
-        </h3>
-      <Link to="/" className="mt-28 text-sm md:text-base text-black hover:text-black underline">Retourner sur la page d&apos;accueil</Link>
+      </h3>
+      <Link
+        to="/"
+        className="mt-28 text-sm md:text-base text-black hover:text-black underline"
+      >
+        Retourner sur la page d&apos;accueil
+      </Link>
     </div>
   );
 }

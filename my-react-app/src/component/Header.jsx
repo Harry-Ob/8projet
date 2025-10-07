fc3a8a7030229e2ee0e70d80b948d3f722bf9012
Harry <ary80@live.fr>
2025-10-07 17:48:38 +0200

    Header.jsx:indentation and clean comments

--- my-react-app/src/component/Header.jsx
+++ my-react-app/src/component/Header.jsx
@@ -1,18 +1,22 @@
-import  { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom"; // Import React Router components
 import logo from "../assets/img/LOGO-header-kai.png";
 // import "../assets/styles/header.scss"
 
-
 function Header() {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
   return (
     <header className="max-w mx-auto px-4 py-10">
       <nav>
         <div className="flex items-center justify-between">
-          <img src={logo} alt="header-logo" className="h-12 w-auto" onClick={() => navigate("/")}/>
+          <img
+            src={logo}
+            alt="header-logo"
+            className="h-12 w-auto"
+            onClick={() => navigate("/")}
+          />
           <div className="flex gap-6 sm:flex-row sm:gap-4 nav_link">
-            <Link to="/" >Accueil</Link>
+            <Link to="/">Accueil</Link>
             <Link to="/about"> A propos</Link>
           </div>
         </div>
